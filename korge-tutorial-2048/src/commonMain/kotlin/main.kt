102575c98bec178fc3db5466bab966212e864ece
phkaa <phkaa@edacom.co.kr>
2021-12-29 22:12:33 +0900

    feat: 게임 화면 UI 배치

--- korge-tutorial-2048/src/commonMain/kotlin/main.kt
+++ korge-tutorial-2048/src/commonMain/kotlin/main.kt
@@ -5,6 +5,8 @@ import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
+import com.soywiz.korma.geom.vector.rect
+import com.soywiz.korma.geom.vector.roundRect
 
 suspend fun main() = Korge(width = 480, height = 640, title = "2048", bgcolor = RGBA(253, 247, 240)) {
 
@@ -20,9 +22,26 @@ suspend fun main() = Korge(width = 480, height = 640, title = "2048", bgcolor = 
 
 	graphics {
 		position(leftIndent, topIndent)
-		RoundRect(10.0, 10.0, cellSize, cellSize, 5.0)
-		/*fill(Colors["#cec0b2"]) {
-			val field = RoundRect
-		}*/
+		fill(Colors["#cec0b2"]) {
+			for ( y in 0..3 ) {
+				for ( x in 0..3 ) {
+					roundRect(10 + (10 + cellSize) * y, 10 + (10 + cellSize) * x, cellSize, cellSize, 5.0)
+				}
+			}
+		}
+	}
+
+	val bgLogo = roundRect(cellSize, cellSize, 5.0, fill = Colors["#edc403"]) {
+		position(leftIndent, 30.0)
+	}
+
+	val bgBest = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+		alignRightToRightOf(bgField)
+		alignTopToTopOf(bgLogo)
+	}
+
+	val bgScore = roundRect(cellSize * 1.5, cellSize * 0.8, 5.0, fill = Colors["#bbae9e"]) {
+		alignRightToLeftOf(bgBest, 24)
+		alignTopToTopOf(bgBest)
 	}
 }
